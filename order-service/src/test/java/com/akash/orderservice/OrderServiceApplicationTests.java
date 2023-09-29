e9cbf7dec6902b8159d76ee8fbe11cf23b1a99c7
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-09-29 19:59:59 +0530

    initialized order service

---
+++ order-service/src/test/java/com/akash/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
