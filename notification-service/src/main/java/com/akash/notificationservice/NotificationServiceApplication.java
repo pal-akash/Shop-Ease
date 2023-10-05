9788be30df4573bd6513f370ff38e6cf9cf19dcc
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-10-05 18:19:57 +0530

    created event driven notification service using kafka

---
+++ notification-service/src/main/java/com/akash/notificationservice/NotificationServiceApplication.java
@@ -0,0 +1,23 @@
+package com.akash.notificationservice;
+
+import com.akash.notificationservice.event.OrderPlacedEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@SpringBootApplication
+@Slf4j
+public class NotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+
+	@KafkaListener(topics = "notificationTopic")
+	public void handleNotification(OrderPlacedEvent orderPlacedEvent){
+		//put the logic for sending out email notification here
+		log.info("Received notification for order - {}", orderPlacedEvent.getOrderNo());
+	}
+
+}
--- order-service/src/main/java/com/akash/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/akash/orderservice/service/OrderService.java
@@ -3,6 +3,7 @@ package com.akash.orderservice.service;
 import com.akash.orderservice.dto.InventoryResponse;
 import com.akash.orderservice.dto.OrderLineItemsDto;
 import com.akash.orderservice.dto.OrderRequest;
+import com.akash.orderservice.event.OrderPlacedEvent;
 import com.akash.orderservice.model.Order;
 import com.akash.orderservice.model.OrderLineItems;
 import com.akash.orderservice.repository.OrderRepository;
@@ -10,6 +11,7 @@ import io.micrometer.tracing.Span;
 import io.micrometer.tracing.Tracer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -29,6 +31,9 @@ public class OrderService {
     private final WebClient.Builder webClientBuilder;
 
     private final Tracer tracer;
+
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
+
 
     public String placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -61,6 +66,7 @@ public class OrderService {
 
             if(allProductsIsInStock){
                 orderRepository.save(order);
+                kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
                 return "Order placed successfully!";
             }else{
                 throw new IllegalArgumentException("Product is not in stock, please try again later!");
